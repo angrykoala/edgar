f7a5ca5321403949cf7f38a2feac326c04312b05
angrykoala <angrykoala@outlook.es>
2017-07-12 01:12:30 +0200

    operator overloading example

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 // sweet_code.js
 import { hi } from './macros/hello_macro.sjs'
+import { + } from './macros/operators.sjs'
 
-hi
+class OperatorTest{
+    constructor(number){
+        this.number=number;
+    }
+    add(otherThing){
+        this.number=this.number+otherThing.number;
+        return this
+    }
+}
+
+let a=new OperatorTest(2)
+let b=new OperatorTest(10)
+
+console.log(a+b)
--- macros/operators.sjs
+++ macros/operators.sjs
@@ -1,1 +1,6 @@
+'lang sweet.js';
 
+
+export operator + left 1 = (left, right) => {
+    return #`checkAdd(${left},${right})`
+};
